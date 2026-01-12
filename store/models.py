97d8f4cceee7c9e44470b85f385f30bf9c838169
FrancisMingomba <mingomba@gmail.com>
2026-01-12 03:28:26 +0000

    WIP

--- store/models.py
+++ store/models.py
@@ -33,37 +33,37 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     date_of_birth = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBER_CHOICES, default='MEMBERSHIP_BRONZE')
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
     
-    class Order(models.Model):
-        PAYMENT_STATUS_PENDING = 'P'
-        PAYMENT_STATUS_COMPLETE = 'C'
-        PAYMENT_STATUS_FAILED = 'F'
-        PAYMENT_STATUS_CHOICES = [
-            (PAYMENT_STATUS_PENDING, 'Pending'),
-            (PAYMENT_STATUS_COMPLETE, 'Complete'),
-            (PAYMENT_STATUS_FAILED, 'Failed')
-        ]
-        
-        placed_at = models.DateTimeField(auto_now_add=True)
-        payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
-        customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
-      
-    class Address(models.Model):
-       street = models.CharField(max_length=255)
-       city = models.CharField(max_length=255)
-       customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-       
-    class OrderItem(models.Model):
-        product = models.ForeignKey(Product, on_delete=models.PROTECT)
-        quantity = models.PositiveSmallIntegerField()
-        unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-        order = models.ForeignKey(Order, on_delete=models.PROTECT)
-        
-    class Cart(models.Model):
-        created_at = models.DateTimeField(auto_now_add=True)
-        customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
-        
-    class CartItem(models.Model):
-        product = models.ForeignKey(Product, on_delete=models.CASCADE)
-        quantity = models.PositiveSmallIntegerField()
-        cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
